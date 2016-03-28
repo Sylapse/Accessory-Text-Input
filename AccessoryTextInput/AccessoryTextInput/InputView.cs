441201b6b56a123f86f31a029fdfd28a17aa0811
Tom Patt <tinglybill@outlook.com>
2016-03-28 18:58:02 +0200

    Added back important line that got deleted. No AutoLayout for the TextField as we just update the frame

--- AccessoryTextInput/AccessoryTextInput/InputView.cs
+++ AccessoryTextInput/AccessoryTextInput/InputView.cs
@@ -14,6 +14,12 @@ namespace AccessoryTextInput
 		{
 		}
 
+		public override void AwakeFromNib ()
+		{
+			base.AwakeFromNib ();
+			TextField.TranslatesAutoresizingMaskIntoConstraints = true;	// No constraints for the TextField, will update it's frame manually
+		}
+
 		public override void LayoutSubviews ()
 		{
 			base.LayoutSubviews ();
