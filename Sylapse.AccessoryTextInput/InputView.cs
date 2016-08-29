6606954aa12b75eb6f68cfc3bd91d976b91e2395
Tom Pratt <tinglybill@outlook.com>
2016-08-29 10:29:50 +0100

    tabs to spaces

--- Sylapse.AccessoryTextInput/InputView.cs
+++ Sylapse.AccessoryTextInput/InputView.cs
@@ -7,50 +7,53 @@ using UIKit;
 
 namespace Sylapse.AccessoryTextInput
 {
-	[Register ("InputView")]
-	public class InputView : UIView
-	{
-		public InputView (IntPtr handle) : base (handle)
-		{
-		}
+    [Register("InputView")]
+    public class InputView : UIView
+    {
+        public InputView(IntPtr handle) : base(handle)
+        {
+        }
 
-		public override void AwakeFromNib ()
-		{
-			base.AwakeFromNib ();
-			TextField.TranslatesAutoresizingMaskIntoConstraints = true;	// No constraints for the TextField, will update its frame manually
-		}
+        public override void AwakeFromNib()
+        {
+            base.AwakeFromNib();
+            TextField.TranslatesAutoresizingMaskIntoConstraints = true; // No constraints for the TextField, will update its frame manually
+        }
 
-		public override void LayoutSubviews ()
-		{
-			base.LayoutSubviews ();
-			TextField.Frame = TextView.Frame;
-		}
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            TextField.Frame = TextView.Frame;
+        }
 
-		[Outlet]
-		public UIButton DoneButton { get; set; }
+        [Outlet]
+        public UIButton DoneButton { get; set; }
 
-		[Outlet]
-		public UITextView TextView { get; set; }
+        [Outlet]
+        public UITextView TextView { get; set; }
 
-		[Outlet]
-		public UITextField TextField { get; set; }
+        [Outlet]
+        public UITextField TextField { get; set; }
 
-		void ReleaseDesignerOutlets ()
-		{
-			if (TextField != null) {
-				TextField.Dispose ();
-				TextField = null;
-			}
+        void ReleaseDesignerOutlets()
+        {
+            if (TextField != null)
+            {
+                TextField.Dispose();
+                TextField = null;
+            }
 
-			if (TextView != null) {
-				TextView.Dispose ();
-				TextView = null;
-			}
+            if (TextView != null)
+            {
+                TextView.Dispose();
+                TextView = null;
+            }
 
-			if (DoneButton != null) {
-				DoneButton.Dispose ();
-				DoneButton = null;
-			}
-		}
-	}
+            if (DoneButton != null)
+            {
+                DoneButton.Dispose();
+                DoneButton = null;
+            }
+        }
+    }
 }
--- Sylapse.AccessoryTextInput/TextInputObject.cs
+++ Sylapse.AccessoryTextInput/TextInputObject.cs
@@ -9,160 +9,170 @@ using System.Linq;
 
 namespace Sylapse.AccessoryTextInput
 {
-	public partial class TextInputObject : UITextField
-	{
-		private InputView _inputView;
-		private Action<string> _callback;
-		private string _hint;
-		private NSLayoutConstraint _maxHeightConstraint;
+    public partial class TextInputObject : UITextField
+    {
+        private InputView _inputView;
+        private Action<string> _callback;
+        private string _hint;
+        private NSLayoutConstraint _maxHeightConstraint;
 
-		private nfloat _maxHeight = 128.0f;
-		public nfloat MaxHeight { 
-			get { return _maxHeight; }
-			set { _maxHeight = value; _maxHeightConstraint.Constant = value; } 
-		}
+        private nfloat _maxHeight = 128.0f;
+        public nfloat MaxHeight
+        {
+            get { return _maxHeight; }
+            set { _maxHeight = value; _maxHeightConstraint.Constant = value; }
+        }
 
-		public new UIColor BackgroundColor {
-			get { return _inputView.BackgroundColor; }
-			set { _inputView.BackgroundColor = value; }
-		}
+        public new UIColor BackgroundColor
+        {
+            get { return _inputView.BackgroundColor; }
+            set { _inputView.BackgroundColor = value; }
+        }
 
-        public UIColor ButtonTextColor {
+        public UIColor ButtonTextColor
+        {
             get { return _inputView.DoneButton.TintColor; }
             set { _inputView.DoneButton.TintColor = value; }
         }
 
-		public TextInputObject()
-		{			
-			var arr = NSBundle.MainBundle.LoadNib ("InputView", null, null);
-			_inputView = Runtime.GetNSObject<InputView> (arr.ValueAt(0));
-			_inputView.TranslatesAutoresizingMaskIntoConstraints = false;
+        public TextInputObject()
+        {
+            var arr = NSBundle.MainBundle.LoadNib("InputView", null, null);
+            _inputView = Runtime.GetNSObject<InputView>(arr.ValueAt(0));
+            _inputView.TranslatesAutoresizingMaskIntoConstraints = false;
 
-			_inputView.TextView.Changed += TextView_Changed;
-			_inputView.DoneButton.TouchUpInside += DoneButton_TouchUpInside;
+            _inputView.TextView.Changed += TextView_Changed;
+            _inputView.DoneButton.TouchUpInside += DoneButton_TouchUpInside;
 
-			SetConstraints ();
-			InputAccessoryView = _inputView;
+            SetConstraints();
+            InputAccessoryView = _inputView;
 
-			BackgroundColor = AccessoryTextInputAppearance.BackgroundColor;
+            BackgroundColor = AccessoryTextInputAppearance.BackgroundColor;
             ButtonTextColor = AccessoryTextInputAppearance.ButtonTextColor;
-		}
+        }
 
-		public void GetInput(string hint, string initialText, Action<string> callback, UIKeyboardType keyboardType = UIKeyboardType.Default, UITextAutocapitalizationType capitalizationType = UITextAutocapitalizationType.Sentences)
-		{
-			_inputView.TextView.Text = initialText;
-			_inputView.TextView.KeyboardType = keyboardType;
-			_inputView.TextView.AutocapitalizationType = capitalizationType;
-			_callback = callback;
-			_hint = hint;
-			SetHint ();
-			DisableScrolling (); // Disable scrolling when the input is shown. It could still be on from the last usage
-			BecomeFirstResponder ();            
-		}
+        public void GetInput(string hint, string initialText, Action<string> callback, UIKeyboardType keyboardType = UIKeyboardType.Default, UITextAutocapitalizationType capitalizationType = UITextAutocapitalizationType.Sentences)
+        {
+            _inputView.TextView.Text = initialText;
+            _inputView.TextView.KeyboardType = keyboardType;
+            _inputView.TextView.AutocapitalizationType = capitalizationType;
+            _callback = callback;
+            _hint = hint;
+            SetHint();
+            DisableScrolling(); // Disable scrolling when the input is shown. It could still be on from the last usage
+            BecomeFirstResponder();
+        }
 
-		public void Finish () 
-		{
-			if (_inputView.TextView.IsFirstResponder) {
-				_callback (_inputView.TextView.Text);
-				_callback = null;
-				ResignFirstResponder ();
-			}
-		}
+        public void Finish()
+        {
+            if (_inputView.TextView.IsFirstResponder)
+            {
+                _callback(_inputView.TextView.Text);
+                _callback = null;
+                ResignFirstResponder();
+            }
+        }
 
-		public void Cancel() 
-		{
-			_callback = null;
-			ResignFirstResponder ();
-		}
+        public void Cancel()
+        {
+            _callback = null;
+            ResignFirstResponder();
+        }
 
-		public override bool BecomeFirstResponder ()
-		{
-			base.BecomeFirstResponder ();
-			_inputView.TextView.BecomeFirstResponder ();
+        public override bool BecomeFirstResponder()
+        {
+            base.BecomeFirstResponder();
+            _inputView.TextView.BecomeFirstResponder();
 
-			// Remove the automatic height constraint on the accessory input view to allow the custom views to size themselves appropriately
-			RemoveAutomaticHeightConstraint ();
-			return true;
-		}
+            // Remove the automatic height constraint on the accessory input view to allow the custom views to size themselves appropriately
+            RemoveAutomaticHeightConstraint();
+            return true;
+        }
 
-		public override bool ResignFirstResponder ()
-		{
-			_inputView.TextView.ResignFirstResponder ();
-			base.ResignFirstResponder ();
-			return true;
-		}
+        public override bool ResignFirstResponder()
+        {
+            _inputView.TextView.ResignFirstResponder();
+            base.ResignFirstResponder();
+            return true;
+        }
 
-		private void DoneButton_TouchUpInside (object sender, EventArgs e)
-		{
-			_callback(_inputView.TextView.Text);	
-			_callback = null;
-			ResignFirstResponder ();
-		}
+        private void DoneButton_TouchUpInside(object sender, EventArgs e)
+        {
+            _callback(_inputView.TextView.Text);
+            _callback = null;
+            ResignFirstResponder();
+        }
 
-		private void TextView_Changed (object sender, EventArgs e)
-		{
-			SetHint ();
+        private void TextView_Changed(object sender, EventArgs e)
+        {
+            SetHint();
 
-			if (_inputView.TextView.ContentSize.Height >= MaxHeight - 16) {	// 16 is two lots of the padding top and bottom of the TextView
-				_inputView.TextView.ScrollEnabled = true;
-			} else if (_inputView.TextView.ScrollEnabled) {
-				DisableScrolling ();
-			}
+            if (_inputView.TextView.ContentSize.Height >= MaxHeight - 16)
+            { // 16 is two lots of the padding top and bottom of the TextView
+                _inputView.TextView.ScrollEnabled = true;
+            }
+            else if (_inputView.TextView.ScrollEnabled)
+            {
+                DisableScrolling();
+            }
 
-			// The height constraint can be added back by iOS due to rotation or backgrounding and foregrounding the app while editing, checking here solves it.
-			RemoveAutomaticHeightConstraint ();
-		}
+            // The height constraint can be added back by iOS due to rotation or backgrounding and foregrounding the app while editing, checking here solves it.
+            RemoveAutomaticHeightConstraint();
+        }
 
-		private void SetHint() 
-		{
-			if (string.IsNullOrEmpty (_inputView.TextView.Text)) {
-				_inputView.TextField.Placeholder = _hint;
-			} else {
-				_inputView.TextField.Placeholder = null;
-			}
-		}
+        private void SetHint()
+        {
+            if (string.IsNullOrEmpty(_inputView.TextView.Text))
+            {
+                _inputView.TextField.Placeholder = _hint;
+            }
+            else {
+                _inputView.TextField.Placeholder = null;
+            }
+        }
 
-		private void DisableScrolling()
-		{
-			_inputView.TextView.ScrollEnabled = false;
-			_inputView.TextView.SizeToFit ();
-		}
+        private void DisableScrolling()
+        {
+            _inputView.TextView.ScrollEnabled = false;
+            _inputView.TextView.SizeToFit();
+        }
 
-		private void SetConstraints() 
-		{
-			_maxHeightConstraint = NSLayoutConstraint.Create (_inputView,
-				NSLayoutAttribute.Height,
-				NSLayoutRelation.LessThanOrEqual,
-				null,
-				NSLayoutAttribute.NoAttribute,
-				0, MaxHeight);
+        private void SetConstraints()
+        {
+            _maxHeightConstraint = NSLayoutConstraint.Create(_inputView,
+                NSLayoutAttribute.Height,
+                NSLayoutRelation.LessThanOrEqual,
+                null,
+                NSLayoutAttribute.NoAttribute,
+                0, MaxHeight);
 
 
-			var minHeightConstraint = NSLayoutConstraint.Create (_inputView,
-				NSLayoutAttribute.Height,
-				NSLayoutRelation.GreaterThanOrEqual,
-				null,
-				NSLayoutAttribute.NoAttribute,
-				0, 44);
+            var minHeightConstraint = NSLayoutConstraint.Create(_inputView,
+                NSLayoutAttribute.Height,
+                NSLayoutRelation.GreaterThanOrEqual,
+                null,
+                NSLayoutAttribute.NoAttribute,
+                0, 44);
 
-			_inputView.AddConstraint (_maxHeightConstraint);
-			_inputView.AddConstraint (minHeightConstraint);
-		}
+            _inputView.AddConstraint(_maxHeightConstraint);
+            _inputView.AddConstraint(minHeightConstraint);
+        }
 
-		// iOS adds a height constraint to accessory input views. In this case it stops the TextView growing so remove it.
-		private void RemoveAutomaticHeightConstraint() 
-		{
-			var accessoryParentConstraints = _inputView.InputAccessoryView.Superview.Constraints;
-			var heightConstraint = accessoryParentConstraints.FirstOrDefault (c => 
-				c.FirstItem == _inputView &&
-				c.FirstAttribute == NSLayoutAttribute.Height &&
-				c.SecondItem == null &&
-				c.SecondAttribute == NSLayoutAttribute.NoAttribute &&
-				c.Relation == NSLayoutRelation.Equal);
+        // iOS adds a height constraint to accessory input views. In this case it stops the TextView growing so remove it.
+        private void RemoveAutomaticHeightConstraint()
+        {
+            var accessoryParentConstraints = _inputView.InputAccessoryView.Superview.Constraints;
+            var heightConstraint = accessoryParentConstraints.FirstOrDefault(c =>
+               c.FirstItem == _inputView &&
+               c.FirstAttribute == NSLayoutAttribute.Height &&
+               c.SecondItem == null &&
+               c.SecondAttribute == NSLayoutAttribute.NoAttribute &&
+               c.Relation == NSLayoutRelation.Equal);
 
-			if (heightConstraint != null) {
-				InputAccessoryView.Superview.RemoveConstraint (heightConstraint);
-			}
-		}
-	}
+            if (heightConstraint != null)
+            {
+                InputAccessoryView.Superview.RemoveConstraint(heightConstraint);
+            }
+        }
+    }
 }
